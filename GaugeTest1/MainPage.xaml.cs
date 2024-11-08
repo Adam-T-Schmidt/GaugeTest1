a1adf8f1a30925c258becc61e139fac6193b06ca
Adam Schmidt <a.s.adam.schmidt@gmail.com>
2024-11-07 22:58:09 -0800

    Added comments to fortify my understanding

--- GaugeTest1/MainPage.xaml.cs
+++ GaugeTest1/MainPage.xaml.cs
@@ -1,7 +1,11 @@
-﻿using GaugeTest1.ViewModel;
-using LiveChartsCore.SkiaSharpView.Extensions;
+﻿/*
+ * Welcome to the main page for this demo application. 
+ * This file handles the Microcharts/Gauge version of the acceleration graph
+ * Enough said so see the following comments for details on how it works
+ */
+
+using GaugeTest1.ViewModel; //define the libraries in use
 using Microcharts;
-//using Microsoft.Maui.Controls.Compatibility.Platform.UWP;
 using SkiaSharp;
 using System.Collections.ObjectModel;
 using System.Timers; 
@@ -10,45 +14,62 @@ namespace GaugeTest1
 {
     public partial class MainPage : ContentPage
     {
-        private System.Timers.Timer _timer = new(100);
-        //private IDispatcherTimer _timer;
+        //creates a timer with an interval of 100 ms to provide a "reason" for the mainpage to access acceleration data
+        private System.Timers.Timer _timer = new(100); 
+        //creates an instance of MainViewModel to allow the file to call functions contained in MainViewModel
         public MainViewModel mainViewModel = new MainViewModel ();
+        //create variables to store acceleration data 
         float accelerationX;
         float accelerationY;
         float accelerationZ;
+        //creates an ObservableCollection of "ChartEntry"s that define a few properties of how the chart will look as well as the data itself 
         public ObservableCollection<ChartEntry> entries;
 
         public MainPage()
         {
-            InitializeComponent ();
+            InitializeComponent (); 
+            //The UI element that contains the chart needs to be passed a "RadialGaugeChart" that contains "ChartEntry"s 
             chartView.Chart = new RadialGaugeChart () { Entries = entries };
+            //calls the function that runs whenever the timer elapses called OnTimedEvent()
             _timer.Elapsed += OnTimedEvent;
+            //sets the timer to re-start every time it elapses
             _timer.AutoReset = true;
+            //enables the timer
             _timer.Enabled = true;
 
+            //sets the ObservableCollection entries equal to a new one that contains placeholder "ChartEntry"s
             entries = new ObservableCollection<ChartEntry>
             {
-                new ChartEntry(0) { Label = "X", ValueLabel = "", Color = SKColor.Parse("#fa0505") },
-                new ChartEntry(0) { Label = "Y", ValueLabel = "", Color = SKColor.Parse("#0511fa") },
-                new ChartEntry(0) { Label = "Z", ValueLabel = "", Color = SKColor.Parse("#15fa05") }
+                new ChartEntry(0) { Label = "X", ValueLabel = "", Color = SKColor.Parse("#fa0505") }, //Label = the name of the data set that is shown next to the chart
+                new ChartEntry(0) { Label = "Y", ValueLabel = "", Color = SKColor.Parse("#0511fa") }, //ValueLabel = the text under the legend label
+                new ChartEntry(0) { Label = "Z", ValueLabel = "", Color = SKColor.Parse("#15fa05") }  //I'm sure you can figure out what Color is lol
             };
-            chartView.Chart = new RadialGaugeChart { Entries = entries };
-
+            //sets the UI element equal to the recently created ObservableCollection (full of dummy data at this point)
+            //If this isn't done, the app will either crash with an InxeXOutOfRangeException or the gauge will not show up
+            chartView.Chart = new RadialGaugeChart { Entries = entries }; 
         }
+
+        //Every time the timer elapses (100ms pass), do the following:
         private void OnTimedEvent(object sender, ElapsedEventArgs t)
         {
+            //create an array of floats to store accelerometer data, then call the function in MainViewModel to access the data
             float[] accValues = mainViewModel.getAccelerationValues ();
+            //set each of the EXISTING ChartEntry locations in the ObservableCollection "entries" equal to their respective XYZ acceleration values
             entries[0] = new ChartEntry (accValues[0]) { Label = "X", ValueLabel = accValues[0].ToString(), Color = SKColor.Parse ("#fa0505") };
             entries[1] = new ChartEntry (accValues[1]) { Label = "Y", ValueLabel = accValues[1].ToString (), Color = SKColor.Parse ("#0511fa") };
             entries[2] = new ChartEntry (accValues[2]) { Label = "Z", ValueLabel = accValues[2].ToString (), Color = SKColor.Parse ("#15fa05") };
 
+            /* Set the chart equal to a new RadialGaugeChart containing the new information
+             * Animations are disabled because this solution re-draws the graph every time.
+             * Microcharts are not designed to update this fast, so they include animations that are only designed to draw once.
+             * They look good, but for fast re-draws they make the data unreadable.*/
             chartView.Chart = new RadialGaugeChart () { Entries = entries, AnimationDuration=new TimeSpan(0), IsAnimated=false, MaxValue=2, MinValue=-2};
         }
+        //honestly useless but i'm keeping it here anyway. Sue me
         protected override void OnDisappearing()
         {
             base.OnDisappearing ();
-            _timer.Stop (); // Stop the timer when the page disappears
-            
+            _timer.Stop ();
         }
     }
 }
--- GaugeTest1/viewmodels/MainViewModel.cs
+++ GaugeTest1/viewmodels/MainViewModel.cs
@@ -1,55 +1,56 @@
-﻿
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿/*
+ * This is the main view model for this simple acceleration graphing application. 
+ * This file collects acceleration data and does the following things with it: 
+ * 
+ * - Prepares the data into an appropriate datatype for displaying in a livechart using data binding
+ * - Provides a function for the main page to gain access to the acceleration data. 
+ * 
+ * Please see the following comments for details on how it works
+ * 
+ */
+using CommunityToolkit.Mvvm.ComponentModel; //declare libraries in use
 using LiveChartsCore;
 using System.Collections.ObjectModel;
-using LiveChartsCore.SkiaSharpView.Extensions;
-using LiveChartsCore.Measure;
 using LiveChartsCore.SkiaSharpView.Painting;
 using SkiaSharp;
 using LiveChartsCore.Defaults;
-
 using LiveChartsCore.SkiaSharpView;
-using Microcharts;
-using Microcharts.Maui;
-using LiveChartsCore.SkiaSharpView.Maui;
-    using System.Windows.Markup;
-using LiveChartsCore.SkiaSharpView.Painting.Effects;
-using Microsoft.Maui;
+
 
 namespace GaugeTest1.ViewModel;
 
 public partial class MainViewModel : ObservableObject
 {
+    //creates "overarching data type" for storing the XY coordinates of acceleration data
+    //this datatype is an observable collection of observable points. Essentially this means it's an array of 
+    //XY coordinates that trigger data binding
+    private static ObservableCollection<ObservablePoint> xyCoordinates = new ObservableCollection<ObservablePoint> (); 
+
+    //Observable property that updates the acceleration lable on the main page
+    [ObservableProperty]
+    string gforceText;
 
 
+    //creates doubles that store acceleration data whenever the reading changes 
+    double accelerationX;
+    double accelerationY;
+    double accelerationZ;
 
 
-
-
-    private static ObservableCollection<ObservablePoint> xyCoordinates = new ObservableCollection<ObservablePoint> ();
-
-
-
-
-
-
+    //creates what is essentially an array of arrays of points that populate the acceleration Livechart (wtf C#?)
     public ISeries[] ScatterSeries { get; set; } = new ISeries[]
     {
         new ScatterSeries<ObservablePoint>
         {
             Name = "XY Coordinates",
             Values=xyCoordinates,
-            //Fill=null,
             GeometrySize=2,
-            //LineSmoothness=0,
             Fill = new SolidColorPaint(SKColors.Blue),
-            //DataLabelsSize = 20,
-            //DataLabelsPaint = new SolidColorPaint(SKColors.Blue),
-            //DataLabelsPosition = LiveChartsCore.Measure.DataLabelsPosition.Top,
-            // DataLabelsFormatter = (point) => "Fuck"+ point.Coordinate.PrimaryValue.ToString("3")+"G",
             Stroke = new SolidColorPaint(SKColors.Blue) { StrokeThickness = 30 }
 
         } };
+    //Creates custom Axes for the livecharts graph
+    //These are data bound to the livecharts graph 
     public Axis[] XAxes { get; set; } = new Axis[]
     {
         new Axis
@@ -62,7 +63,7 @@ public partial class MainViewModel : ObservableObject
             MaxLimit=1.5,
             MinLimit=-1.5,
             SeparatorsPaint = new SolidColorPaint(SKColors.LightSlateGray) { StrokeThickness = 2 }
-                }
+        }
     };
     public Axis[] YAxes { get; set; } = new Axis[]
     {
@@ -76,75 +77,58 @@ public partial class MainViewModel : ObservableObject
             MaxLimit=1.5,
             MinLimit=-1.5,
             SeparatorsPaint = new SolidColorPaint(SKColors.LightSlateGray) { StrokeThickness = 2 }
-                }
+        }
     };
 
-    [ObservableProperty]
-    string gforceText;
-
-    double accelerationX;
-    double accelerationY;
-    double accelerationZ; 
-
-
-
-
-
-
-
-
-
+    //Start the accelerometer
     public MainViewModel()
     {
         StartAccelerometer ();
-        
-
-
-
-
     }
 
-
+    //function to allow the main page to access acceleration data.
+    //returns an array[3] of floats that contain X, Y, Z acceleration data in that order
     public float[] getAccelerationValues()
     {
-        //Console.WriteLine ("getAccelerationValues() Called");
         float[] accelerationValues = new float[3];
         accelerationValues[0] = (float)accelerationX; 
         accelerationValues[1] = (float)accelerationY;
         accelerationValues[2] = (float)accelerationZ;
         return accelerationValues; 
     }
-
-
+    //the title is pretty self explanatory 
     private void StartAccelerometer()
     {
         try
         {
-            Accelerometer.ReadingChanged += OnAccelerometerReadingChanged;
-            Accelerometer.Start (SensorSpeed.UI);
+            Accelerometer.ReadingChanged += OnAccelerometerReadingChanged; // this line triggers the OnAccelerometerReadingChanged() function
+            Accelerometer.Start (SensorSpeed.UI); 
         }
         catch (FeatureNotSupportedException) { }
         catch (Exception e) { }
     }
 
+    //this function updates the acceleration data and does the following: 
     private void OnAccelerometerReadingChanged(object sender, AccelerometerChangedEventArgs e)
     {
-        //Console.WriteLine(xyCoordinates.Count);
-        //xyCoordinates.Clear();
-        xyCoordinates.Add (new ObservablePoint (e.Reading.Acceleration.X, e.Reading.Acceleration.Y));
-        if (xyCoordinates.Count >= 10)
+        //creates a new observable point and appends it to the "overarching data type" described above
+        xyCoordinates.Add (new ObservablePoint (e.Reading.Acceleration.X, e.Reading.Acceleration.Y)); 
+        //This if-statement determines the number of points on the graph, or in other words, the amount of "trail" the graph has
+        if (xyCoordinates.Count >= 10) //if the number of ObservablePoints in the "Overarching data type" is >= 10:
         {
-            xyCoordinates.RemoveAt(0);
+            //remove the first element
+            xyCoordinates.RemoveAt(0); 
+            //an ObservableCollection automatically shifts the rest of the data set down one index to fill the empty space which is handy. 
         }
-        GforceText = $"X: {e.Reading.Acceleration.X:F2}, Y: {e.Reading.Acceleration.Y:F2}, Z: {e.Reading.Acceleration.Z:F2}";
+        //update the label on the mainpage that shows raw acceleration data
+        GforceText = $"X: {e.Reading.Acceleration.X:F2}, Y: {e.Reading.Acceleration.Y:F2}, Z: {e.Reading.Acceleration.Z:F2}"; 
+        //updates the above double values to contain the most recent acceleration data
         accelerationX = e.Reading.Acceleration.X; 
         accelerationY = e.Reading.Acceleration.Y;
         accelerationZ = e.Reading.Acceleration.Z;
     }
 
-
-
-
+    //function to stop accelerometer data from being read. Not being used in this application but will be useful in others
     private void StopAccelerometer()
     {
         Accelerometer.Stop ();
